05e22e9e19c63181e2439b480bad1e6205966b39
Huy Hoang <huy@iplayup.com>
2011-08-08 21:19:14 +1000

    Added description

--- src/problem6.rb
+++ src/problem6.rb
@@ -1,5 +1,13 @@
-#If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
-#Find the sum of all the multiples of 3 or 5 below 1000.
+#The sum of the squares of the first ten natural numbers is,
+#
+#12 + 22 + ... + 102 = 385
+#The square of the sum of the first ten natural numbers is,
+#
+#(1 + 2 + ... + 10)2 = 552 = 3025
+#Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025  385 = 2640.
+#
+#Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+
 class Problem6
   def self.difference max_num
     result = 0
